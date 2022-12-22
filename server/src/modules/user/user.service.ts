5ef74412e4a4e05ebf998941be365628f4f535f6
nguyen kien <nkien6962@gmail.com>
2022-12-22 18:41:46 +0700

    fix update user

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,15 +1,12 @@
 import { UserRepository } from './user.repository';
-import { LoggerService } from './../logger/custom.logger';
+import { LoggerService } from '../../logger/custom.logger';
 import { BaseService } from './../../base/base.service';
-import { RegisterRequestDto } from './../auth/dto/register-request.dto';
-import { Injectable, UnauthorizedException, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UserEntity } from './entities/user.entity';
-import { DataSource, ObjectID, UpdateResult } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { InjectDataSource } from '@nestjs/typeorm';
-import { EntityId } from 'typeorm/repository/EntityId';
 import { UploadService } from '../upload-file/upload-file.service';
 import * as bcrypt from 'bcrypt';
-// import { UpdateUserDto } from 'src/base/base.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UserService extends BaseService<UserEntity, UserRepository> {
@@ -22,32 +19,34 @@ export class UserService extends BaseService<UserEntity, UserRepository> {
     super(repository, logger);
   }
 
+  testABC(user) {
+    console.log('user', user);
+
+    return user;
+  }
+
   findByEmail(email: string): Promise<UserEntity | null> {
     return this.repository.findOne({ where: { email: email } });
   }
 
   async updateRefreshToken(filter, Token) {
-    if (Token.refreshToken) {
-      Token.refreshToken = await bcrypt.hash(this.reverseToken(Token.refreshToken), 10);
-    }
+    // if (Token.refreshToken) {
+    //   Token.refreshToken = await bcrypt.hash(this.reverseToken(Token.refreshToken), 10);
+    // }
     return await this._update(filter, Token);
   }
 
-  async updateUser(updateUserDto: UpdateUserDto, file): Promise<UserEntity> {
-    // const userId = updateUserDto.userId;
-    const findUser = await this._findById(updateUserDto.id);
-    // console.log('update profile', userId);
-    // const updateUser = new UserEntity(updateUserDto);
+  async updateUser(userId, updateUserDto: UpdateUserDto, file): Promise<UserEntity> {
+    const findUser = await this._findById(userId.payload.id);
+    console.log('update profile', userId);
+    const updateUser = new UserEntity(updateUserDto);
+
     const filepath = await this.uploadService.createFile(file);
     if (filepath) {
-      updateUserDto.avatar = filepath;
+      updateUser.avatar = filepath;
     }
-    console.log('updateUserDto.id', updateUserDto.id);
-    console.log(findUser);
-    console.log('filepath', filepath);
-
-    return this._update(updateUserDto.id, updateUserDto);
-    return;
+    return await this._update(userId.payload.id, updateUser);
+    // return findUser;
   }
 
   reverseToken(plainText: string) {
