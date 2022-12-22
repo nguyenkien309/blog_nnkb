b79d3994d462003068503ee5ee57c07f6f0321f0
nguyen kien <nkien6962@gmail.com>
2022-12-22 18:42:51 +0700

    fix refresh token

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AuthResponse } from '../types/auth-response';
 
 //export const API_URL = "https://deploy-nest-react-blog-app.herokuapp.com";
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   // withCredentials: true,
@@ -13,7 +13,6 @@ api.interceptors.request.use((config) => {
   // @ts-ignore
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   console.log('token config', localStorage.getItem('token'));
-
   return config;
 });
 
@@ -23,18 +22,27 @@ api.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
+
     if (
       error.response.status === 401 &&
       error.config &&
       !error.config._isRetry
     ) {
+      localStorage.removeItem('token');
       originalRequest._isRetry = true;
       try {
-        const response = await axios.get<AuthResponse>(
+        const refresh_token = localStorage.getItem('refreshToken');
+        console.log('getRT from http', refresh_token);
+        console.log('POST RET');
+
+        const response = await axios.post<AuthResponse>(
           `${API_URL}/v1/auth/refresh`,
-          { withCredentials: true }
+          { refresh_token }
+          // { withCredentials: true }
         );
+        console.log('response INDEX.TS', response);
         localStorage.setItem('token', response.data.accessToken);
+        console.log('response.data.accessToken', response.data.accessToken);
 
         return api.request(originalRequest);
       } catch (e) {
